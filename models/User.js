d59ffe92227c29b6aca4b2da68d41d18ab692790
Travis <travis3214@yahoo.com>
2023-08-24 17:25:33 -0400

    built the model for user

--- models/User.js
+++ models/User.js
@@ -1,1 +1,46 @@
+// Requires mongoose to build the models
 const { Schema, model, Types } = require('mongoose');
+
+// Builds a model for the users
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address'],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought",
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+  
+  // This retrieves the length of the user's friends array field on query
+  userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
+
+  const User = model('User', userSchema);
+  module.exports = User;
