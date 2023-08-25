7676db64b15cb7bfe97644d6da47f99af958ff30
Travis <travis3214@yahoo.com>
2023-08-24 23:38:03 -0400

    forgot to import the rest of the user controllers after making them

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,12 +6,19 @@ const {
     getUsers,
     getSingleUser,
     createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
 } = require("../../controllers/userController.js");
 
 // /api/users
 router.route("/").get(getUsers).post(createUser);
 
 // /api/users/:id
-router.route("/:userId").get(getSingleUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
